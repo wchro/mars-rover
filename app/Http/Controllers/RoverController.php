c0321ae0103f09fb70f4c074a9d2dd53c8c79c6b
wchro <93561159+wchro@users.noreply.github.com>
2025-07-02 03:02:33 +0200

    Added Rover Controller with a basic logic

--- app/Http/Controllers/RoverController.php
+++ app/Http/Controllers/RoverController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 class RoverController extends Controller
 {
     public function move(Request $request) {
-        $data = $request->validate([]);
+        $data = $request->validate([
+            'x'         => 'required|integer|min:0|max:199',
+            'y'         => 'required|integer|min:0|max:199',
+            'direction' => 'required|string|in:N,S,E,W',
+            'commands'  => 'required|string'
+        ]);
 
         // respuesta de ejemplo
-        return response()->json(['status' => "OK"]);
+        return response()->json(['info' => $data]);
     }
 }
