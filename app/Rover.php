1dfb9c7f86e0ef3078b3817449e6329bec34bb4a
wchro <93561159+wchro@users.noreply.github.com>
2025-07-02 03:40:51 +0200

    Created Rover class with unfinished logic

---
+++ app/Rover.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App;
+
+class Rover
+{
+    public $x;
+    public $y;
+    public $direction;
+    public $map_size = 200;
+    public $obstacles = [];
+
+    public function __construct($x, $y, $direction)
+    {
+        $this->x = $x;
+        $this->y = $y;
+        $this->direction = $direction;
+        $this->generate_obstacles();
+    }
+
+    // genermaos obstaculos random
+    private function generate_obstacles()
+    {
+        $this->obstacles = [
+            ["x" => 5, "y" => 10],
+            ["x" => 15, "y" => 11],
+            ["x" => 20, "y" => 30]
+        ];
+    }
+
+    public function move($commands)
+    {
+        $commands = strtoupper($commands); // convertimos todas las letras en mayusculas
+        $commands = str_split($commands); // separamos los comandos
+
+        foreach ($commands as $command) {
+            if ($command === 'F') {
+                // a. obstaculo?
+                if (!$this->can_move_forward()) {
+                    return [
+                        'x' => $this->x,
+                        'y' => $this->y,
+                        'direction' => $this->direction,
+                        'obstacle' => true,
+                        'message' => 'Obstacle detected, movement stopped 🚫'
+                    ];
+                }
+                // b. mover
+            } else if ($command === 'R') {
+                // RIGHT
+            } else if ($command === 'L') {
+                // LEFT
+            }
+        }
+
+        return [];
+    }
+
+    private function can_move_forward()
+    {
+        $next_x = $this->x;
+        $next_y = $this->y;
+
+        // simulamos movimiento
+        switch ($this->direction) {
+            case 'N': $next_y++; break;
+            case 'S': $next_y--; break;
+            case 'E': $next_x++; break;
+            case 'W': $next_x++; break;
+        }
+
+        // comprobar si existe obstaculo
+        foreach ($this->obstacles as $obstacle) {
+            if ($obstacle['x'] === $next_x && $obstacle['y'] === $next_y) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private function move_forward() {}
+
+    private function turn_right() {}
+
+    private function turn_left() {}
+
+
+}
